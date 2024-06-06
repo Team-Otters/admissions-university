ae1342ae2017f42c9abb07d893aa42d92f519973
nyb1711 <nyb1711@gmail.com>
2024-06-06 07:00:56 +0700

    add final api

--- src/app/(authentication)/(PhongKhaoThi)/scoreManage/[id]/page.tsx
+++ src/app/(authentication)/(PhongKhaoThi)/scoreManage/[id]/page.tsx
@@ -7,12 +7,14 @@ import { FaPencil } from "react-icons/fa6";
 import { MdDelete } from "react-icons/md";
 import useDebounce from "@/hooks/useDebounce";
 import FormPaper from "@/components/formPaper";
+import axios from "axios";
+import { host } from "@/constants/string";
 
-const ScoreManagePage: React.FC = ({ params}) => {
-  const {id} = params;
+const ScoreManagePage: React.FC = ({ params }) => {
+  const { id } = params;
   const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
   const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
-  const [rowToEdit, setRowToEdit] = React.useState<ScoreManageForm>();
+  const [rowToEdit, setRowToEdit] = React.useState<Paper>();
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const [isScoreInput, setIsScoreInput] = useState<boolean>(false);
   const [filterGroupList, setFilterGroupList] = useState([
@@ -20,75 +22,26 @@ const ScoreManagePage: React.FC = ({ params}) => {
     "Mã bài thi",
     "Tên thí sinh",
     "Môn thi",
-    "Ngày thi",
     "Điểm",
   ]);
-  const [paperContainers, setPaperContainers] = useState<ExamManageForm[]>([
-    {
-      paperContainerCode: "T001",
-      roomName: "B5.08",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      numberOfPapers: 40,
-    },
-    {
-      paperContainerCode: "T002",
-      roomName: "B1.14",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      numberOfPapers: 40,
-    },
-    {
-      paperContainerCode: "T002",
-      roomName: "B4.14",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      numberOfPapers: 40,
-    },
-  ]);
-  const [isValid, setIsValid] = React.useState<boolean>(false) 
+  const [paperContainers, setPaperContainers] = useState<PaperContainer[]>([]);
+  const [isValid, setIsValid] = React.useState<boolean>(false);
   const [recentFilterGroupList, setRecentFilterGroupList] =
     useState(filterGroupList);
   const [searchText, setSearchText] = React.useState<string>("");
-  const [papers, setPapers] = useState<ScoreManageForm[]>([
-    {
-      paperCode: "B001",
-      studentCode: "SV001",
-      studentName: "Nguyễn Văn A",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      score: 10,
-    },
-    {
-      paperCode: "B002",
-      studentCode: "SV001",
-      studentName: "Nguyễn Văn AA",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      score: 10,
-    },
-    {
-      paperCode: "B003",
-      studentCode: "SV001",
-      studentName: "Nguyễn Văn A",
-      subject: "Hóa Học",
-      date: "06/06/2024",
-      score: 10,
-    },
-  ]);
+  const [papers, setPapers] = useState<Paper[]>([]);
+  const [students, setStudents] = useState<Student[]>([]);
 
-  const [searchPapers, setSearchPapers] =
-    React.useState<ScoreManageForm[]>(papers);
+  const [searchPapers, setSearchPapers] = React.useState<Paper[]>([]);
 
-  const search = (text: string): ScoreManageForm[] => {
-    let temp: ScoreManageForm[] = papers.filter((paper) => {
+  const search = (text: string): Paper[] => {
+    let temp: Paper[] = papers.filter((paper) => {
       for (const element of recentFilterGroupList) {
         switch (element) {
           case "All":
             console.log(text.toLowerCase());
             if (
-              paper.paperCode.toLowerCase().includes(text.toLowerCase()) ||
-              paper.studentName.toLowerCase().includes(text.toLowerCase()) ||
+              paper.student.toLowerCase().includes(text.toLowerCase()) ||
               paper.subject.toLowerCase().includes(text.toLowerCase()) ||
               paper.date.toLowerCase().includes(text.toLowerCase()) ||
               paper.score.toString().includes(text)
@@ -129,6 +82,50 @@ const ScoreManagePage: React.FC = ({ params}) => {
     return temp;
   };
 
+  const getAllStudents = async (id: string) => {
+    console.log(localStorage.getItem("accessToken"));
+    try {
+      let token = localStorage.getItem("accessToken");
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `${host}paper-containers/paper/${id}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.request(config);
+      //createUser(newUser);
+      setStudents(response.data);
+      console.log("td: ", response.data);
+      // Handle successful login based on your API's response structure
+    } catch (error) {
+      console.error(error); // Handle errors appropriately (e.g., display error messages)
+    }
+  };
+
+  const getAllPaper = async () => {
+    console.log(localStorage.getItem("accessToken"));
+    try {
+      let token = localStorage.getItem("accessToken");
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `${host}paper`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.request(config);
+      //createUser(newUser);
+      setPapers(response.data);
+      console.log("pp: ", response.data);
+      // Handle successful login based on your API's response structure
+    } catch (error) {
+      console.error(error); // Handle errors appropriately (e.g., display error messages)
+    }
+  };
+
   const applyFilter = (): void => {
     let selectElement = document.getElementById("filter") as HTMLSelectElement;
     console.log(selectElement.value);
@@ -165,53 +162,96 @@ const ScoreManagePage: React.FC = ({ params}) => {
     setSearchText(e.target.value);
   };
 
-  const handleClearRow = (paper: ScoreManageForm): void => {
-    let temp: ScoreManageForm[] = [...papers];
-    temp = temp.filter((value) => {
-      if (value.paperCode !== paper.paperCode) {
-        return value;
-      }
-    });
-    setPapers(temp);
-  };
-
-  const handleSubmit = (data: ScoreManageForm): void => {
-    let temp = [...papers];
-    temp.push(data);
-    setPapers(temp);
-  };
-
-  const handleEdit = (data: ScoreManageForm): void => {
-    let idx: number = -1;
-    papers.map((paper, index) => {
-      if (paper.paperCode == data.paperCode) {
-        idx = index;
-      }
-    });
-
-    if (idx != -1) {
-      let temp: ScoreManageForm[] = [...papers];
-      temp[idx] = data || {
-        paperCode: "",
-        studentCode: "",
-        studentName: "",
-        subject: "",
-        date: "",
-        score: 0,
+  const handleClearRow = async (id: string) => {
+    try {
+      let token = localStorage.getItem("accessToken");
+      let dt = JSON.stringify({
+        id: id,
+      });
+      let config = {
+        method: "delete",
+        maxBodyLength: Infinity,
+        url: `${host}paper/${id}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: dt,
       };
-      setPapers(temp);
-      console.log(temp[idx]);
-    } else {
-      console.log("Lỗi update");
+      const response = await axios.request(config).then((response) => {
+        getAllPaper();
+      });
+      //createUser(newUser);
+      // Handle successful login based on your API's response structure
+    } catch (error) {
+      console.error(error); // Handle errors appropriately (e.g., display error messages)
     }
   };
 
-  const handleEditRow = (paper: ScoreManageForm): void => {
+  const handleSubmit = async (data: Paper) => {
+    try {
+      let token = localStorage.getItem("accessToken");
+      let dt = JSON.stringify({
+        studentId: data.student,
+        subjectId: data.subject,
+        numberOfPapers: 1,
+        score: data.score,
+      });
+      let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${host}paper`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        data: dt,
+      };
+      const response = await axios.request(config).then((response) => {
+        getAllPaper();
+      });
+      //createUser(newUser);
+      // Handle successful login based on your API's response structure
+    } catch (error) {
+      console.error(error); // Handle errors appropriately (e.g., display error messages)
+    }
+  };
+
+  const handleEdit = async (data: Paper) => {
+    try {
+      let dt = JSON.stringify({
+        studentId: data.student,
+        subjectId: data.subject,
+        numberOfPapers: 1,
+        score: data.score,
+      });
+      let token = localStorage.getItem("accessToken");
+      let config = {
+        method: "put",
+        maxBodyLength: Infinity,
+        url: `${host}paper/${data.id}`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        data: dt,
+      };
+      const response = await axios.request(config).then((response) => {
+        getAllPaper();
+      });
+    } catch (error) {
+      console.error(error); // Handle errors appropriately (e.g., display error messages)
+    }
+  };
+
+  const handleEditRow = (paper: Paper): void => {
     setRowToEdit(paper);
     setIsOpenForm(true);
   };
 
-  const handleUpdateScore = (): void => {};
+  useEffect(() => {
+    getAllPaper();
+    getAllStudents(id);
+  }, []);
 
   return (
     <Container
@@ -222,10 +262,9 @@ const ScoreManagePage: React.FC = ({ params}) => {
       <div className="p-1 mb-4">
         <h2 className="text-3xl">{"Quản lý chấm thi > Xem túi bài thi"}</h2>
         <div className=" w-11/12 mx-auto font-notoSans font-bold mt-6 align-center text-lg">
-          Mã túi bài thi:
+          {`Mã túi bài thi: ${id}`}
         </div>
-        <div className="w-11/12 mx-auto flex flex-row ">
-          <div className=" flex-1 flex-row ">
+        <div className="w-11/12 mx-auto flex flex-row justify-between">
           <div
             className={`w-1/3 bg-white overflow-hidden h-12 rounded-3xl px-2 border-black border ${
               isInputFocused ? "border-2" : "border-1"
@@ -244,7 +283,7 @@ const ScoreManagePage: React.FC = ({ params}) => {
             ></input>
             <FaSearch className="w-2/12 lg:w-1/12 self-center" />
           </div>
-          <div className="flex">
+          <div className="flex flex-row">
             <MdOutlineFilterAlt className="self-center ml-2" size={24} />
             <select
               name="filter"
@@ -261,22 +300,26 @@ const ScoreManagePage: React.FC = ({ params}) => {
               })}
             </select>
           </div>
-          </div>
 
-          <Button className={`${isScoreInput? ` bg-lime-700` : ``} mr-0`}
+          <Button
+            className={`${isScoreInput ? ` bg-lime-700` : ``} mr-0`}
             onClick={() => {
-              if (isScoreInput) {
-                handleUpdateScore();
-              }
-              setIsScoreInput(!isScoreInput);
+              setRowToEdit(undefined);
+              setIsEdit(false);
+              setIsOpenForm(true);
             }}
+            // onClick={() => {
+            //   if (isScoreInput) {
+            //     handleUpdateScore();
+            //   }
+            //   setIsScoreInput(!isScoreInput);
+            // }}
           >
-            {isScoreInput ? "Lưu điểm" : "Nhập điểm"}
+            Thêm bài thi
           </Button>
-
         </div>
         <div className="w-11/12 mx-auto font-notoSans font-bold mt-6 align-center text-lg">
-          Tổng: {searchPapers.length}
+          Tổng: {searchPapers?.length}
         </div>
         <table className="max-w-11/12 w-11/12 mx-auto text-lg shadow-tableShadow border-collapse rounded-3xl bg-white">
           <thead>
@@ -285,23 +328,22 @@ const ScoreManagePage: React.FC = ({ params}) => {
               <th className="border-l border-gray p-2 w-2/12">Mã bài thi</th>
               <th className="border-l border-gray p-2">Tên thí sinh</th>
               <th className="border-l border-gray p-2">Tên môn thi</th>
-              <th className="border-l border-gray p-2">Ngày thi</th>
               <th className="border-l border-gray p-2 w-1/12">Điểm</th>
               <th className="w-12 border-gray p-2"></th>
             </tr>
           </thead>
           <tbody>
-            {searchPapers.map((item, index) => {
+            {searchPapers?.map((item, index) => {
               return (
                 <tr
                   className="border-b border-gray rounded-b-lg last:border-none"
                   key={index}
                 >
                   <td className="px-2 py-1 border-r">{index + 1}</td>
-                  <td className="px-2 py-1 border-r">{item.paperCode}</td>
-                  <td className="px-2 py-1 border-r">{item.studentName}</td>
-                  <td className="px-2 py-1 border-r">{item.subject}</td>
-                  <td className="px-2 py-1 border-r">{item.date}</td>
+                  <td className="px-2 py-1 border-r">{item.id}</td>
+                  <td className="px-2 py-1 border-r">{item.student?.name}</td>
+                  <td className="px-2 py-1 border-r">{item.subject?.name}</td>
+                  <td className="px-2 py-1 border-r">{item.score}</td>
                   <td className="px-2 py-1">
                     <input
                       className="bg-white"
@@ -317,7 +359,12 @@ const ScoreManagePage: React.FC = ({ params}) => {
                       className="cursor-pointer"
                       onClick={() => {
                         setIsEdit(true);
-                        handleEditRow(item);
+                        handleEditRow({
+                          id: item.id,
+                          student: item.student?.id,
+                          subject: item.subject?.id,
+                          score: item.score,
+                        });
                       }}
                     >
                       <FaPencil size={18} />
@@ -340,17 +387,16 @@ const ScoreManagePage: React.FC = ({ params}) => {
           <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
             <FormPaper
               papers={papers}
+              students={students}
               closeModal={() => setIsOpenForm(false)}
               isEdit={isEdit}
               onSubmit={isEdit ? handleEdit : handleSubmit}
               defaultValue={
                 rowToEdit === undefined
                   ? {
-                      paperCode: "",
-                      studentCode: "",
-                      studentName: "",
+                      id: "",
+                      student: students[0]?.id,
                       subject: "",
-                      date: "",
                       score: 0,
                     }
                   : rowToEdit
